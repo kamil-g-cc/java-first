7adbf3b26a693a337d779211a8dee069695e86ac
Kamil Grondys <kgrondys@gmail.com>
2021-05-09 14:29:47 +0200

    adding String formater

--- src/main/java/com/codecool/MyApp/App.java
+++ src/main/java/com/codecool/MyApp/App.java
@@ -10,27 +10,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Wybierz pozycje z menu");
-        System.out.println("1. start");
-        System.out.println("2. zakoncz");
-
-        Scanner scanner = new Scanner(System.in);
-        //int choice = scanner.nextInt();
-        String response = scanner.nextLine();
-        int choice = 0;
-        System.out.println(response);
-        if(response.equals("1")){
-            choice = 1;
-        } else if(response.equals("2")){
-            choice = 2;
-        } else {
-            System.out.println("Nieprawidlowy wybor");
-        }
-
-        if(choice == 1){
-            System.out.println("wybrano start");
-        } else if(choice==2){
-            System.out.println("wyborano koniec");
-        }
+        Bicykle bicykle = new Bicykle(20, 90, 2);
+        bicykle.setSpeed(25);
+        bicykle.setGear(3);
+        bicykle.setCadence(bicykle.getCadence()+5);
+        System.out.println(bicykle.print());
+        System.out.println(bicykle.alternativePrint());
     }
 }
--- src/main/java/com/codecool/MyApp/Bicykle.java
+++ src/main/java/com/codecool/MyApp/Bicykle.java
@@ -11,7 +11,36 @@ public class Bicykle {
         gear = initialGear;
     }
 
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public int getCadence() {
+        return cadence;
+    }
+
+    public void setCadence(int cadence) {
+        this.cadence = cadence;
+    }
+
+    public void setGear(int gear) {
+        this.gear = gear;
+    }
+
     public int getGear() {
         return gear;
     }
+
+    public String print(){
+        return "Moj rower porusza sie z predkoscia "+speed+" km/h z kandencja "+cadence+" i na biegu "+gear;
+    }
+
+    public String alternativePrint(){
+        return String.format("Moj rower porusza sie z predkoscia %d z kandencja %d i na biegu %d", speed, cadence, gear);
+    }
+
 }
