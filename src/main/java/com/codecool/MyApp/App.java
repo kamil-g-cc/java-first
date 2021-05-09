a05bb5c70db3e53b11454ac48642d925abfd0587
Kamil Grondys <kgrondys@gmail.com>
2021-05-09 14:03:45 +0200

    adding array of Bicykles

--- src/main/java/com/codecool/MyApp/App.java
+++ src/main/java/com/codecool/MyApp/App.java
@@ -9,6 +9,15 @@ public class App
     public static void main( String[] args )
     {
         Bicykle bicykle = new Bicykle(25, 85, 4);
+        Bicykle[] arrayOfBicykles = new Bicykle[10];
+
+        for(int i = 0 ; i < 10; ++i){
+            arrayOfBicykles[i] = new Bicykle(20, 80, 3);
+        }
+        for(Bicykle b : arrayOfBicykles){
+            System.out.println(b.getGear());
+        }
+
         System.out.println(bicykle.getGear());
     }
 }
