0b61c480fb70400316f5a170f821209508556a75
Kamil Grondys <kgrondys@gmail.com>
2021-05-09 14:39:24 +0200

    adding support for line arguments

--- src/main/java/com/codecool/MyApp/App.java
+++ src/main/java/com/codecool/MyApp/App.java
@@ -10,13 +10,9 @@ public class App
 {
     public static void main( String[] args )
     {
-        Bicykle bicykle = new Bicykle(20, 90, 2);
-        bicykle.setSpeed(25);
-        bicykle.setGear(3);
-        bicykle.setCadence(bicykle.getCadence()+5);
-        System.out.println(bicykle.print());
-        System.out.println(bicykle.alternativePrint());
-
-        System.out.println(Bicykle.numberOfBicykles);
+        System.out.println(args.length);
+        for(String a: args){
+            System.out.println(a);
+        }
     }
 }
